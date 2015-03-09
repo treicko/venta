d66db9199355597af45892b423246ad798cd7f50
Leonardo <leo_mejia_p@hotmail.com>
2015-03-09 13:29:38 -0400

    agregando una venta de un articulo y calcular su precio

---
+++ lib/venta.rb
@@ -0,0 +1,16 @@
+class Venta
+
+	def initilize()
+		@total = 0
+	end
+
+	def aniadir(tipo_producto, nombre_producto, precio, cantidad)
+		@total = precio
+	end
+
+	def calcular_total
+		@total
+	end
+
+
+end
--- spec/venta_test_spec.rb
+++ spec/venta_test_spec.rb
@@ -1,5 +1,10 @@
 require_relative "spec_helper";
+require_relative "../lib/venta";
 
-describe "Prueba Spec" do
-	it "Primera Prueba"
+describe Venta do
+	it "devuelve el total para una venta con un articulo" do
+		venta = Venta.new();
+		venta.aniadir("articulo","teclado",2,1);
+		venta.calcular_total.should == 2
+	end
 end
